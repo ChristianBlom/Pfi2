3f383aa5303d4e8acc9695397e26b02c86ac31dc
ChristianBlom <Christian.blom@live.se>
2015-03-04 06:28:42 +0100

    Update Human.java
    Klar

--- A4_1_Christian_Blom/src/Uppgift_4/Human.java
+++ A4_1_Christian_Blom/src/Uppgift_4/Human.java
@@ -4,30 +4,30 @@ public class Human {
 	private Dog dog;
 	private String name;
 	private String Info = "";
-	
-	
+
+
 	public Human (String name){
 		this.name = name;
-		}
+	}
 	public String getName(){
 		return name;
 	}
 	public void buyDog(Dog dog){
 		this.dog = dog;
-	 
+
 	}
 	public String getInfo() {
-		
+
 		if (dog == null ){
 			String Info = name +(" har ingen hund ");
 			return Info;
+		}
+		if(name == null){
+			String info = (" Här finns varken hund eller människa");
+			return info;
 		}else {
-			String Info = name +(" Äger en hund some heter ")+ name;
+
+			String Info = name +(" äger en hund some heter ")+ name;
+
 			return Info;
 		}
-
-	
-	}
-
-
-}	
